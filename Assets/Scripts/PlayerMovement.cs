ca95b780d3727fdf2d1e23a0f5ef393bba8ecff1
fr4nz1-4 <captain.proton.paris@gmail.com>
2025-05-20 22:38:48 +0200

    character now shrinks when moving

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -26,10 +26,21 @@ public class PlayerMovement : MonoBehaviour
         if (isMoving)
         {
             transform.position = Vector3.MoveTowards(transform.position, targetPosition, moveSpeed * Time.deltaTime);
+            
+            // Skalierung anhand der y-Position
+            float minY = 0.5f; 
+            float maxY = -2f;
+
+            float scale = Mathf.InverseLerp(minY, maxY, transform.position.y); // Wert zwischen 0 und 1
+            float finalScale = Mathf.Lerp(0.35f, 0.5f, scale); // Skaliere zwischen 60% und 100%
+
+            transform.localScale = new Vector3(finalScale, finalScale, 0.5f); // gleichmäßig skalieren
+            
             if (Vector3.Distance(transform.position, targetPosition) < 0.1f)
             {
                 isMoving = false;
             }
+            
         }
     }
 }
