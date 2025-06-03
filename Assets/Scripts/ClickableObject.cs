beb89db57898e9801e2715f7b7e5e88a17a78752
fr4nz1-4 <captain.proton.paris@gmail.com>
2025-06-04 00:33:29 +0200

    add audio to the train (bug: plays audio once when the game is started)

--- Assets/Scripts/ClickableObject.cs
+++ Assets/Scripts/ClickableObject.cs
@@ -5,10 +5,26 @@ public class ClickableObject : MonoBehaviour
 {
     public MonologScript monologScript;
     public string text;
+    private AudioSource audioSource;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
 
     private void OnMouseDown()
     {
         Debug.Log("You clicked on: " + gameObject.name);
-        monologScript.ShowMonolog(text);
+        
+        if (monologScript != null)
+        {
+            monologScript.ShowMonolog(text);
+        }
+
+        if (audioSource != null)
+        {
+            Debug.Log("play audiosource");
+            audioSource.Play();
+        }
     }
 }
