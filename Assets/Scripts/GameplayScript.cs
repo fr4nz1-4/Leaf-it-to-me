022b5fd2d3764d1fe749a0df1e4b83644cb81b17
fr4nz1-4 <captain.proton.paris@gmail.com>
2025-06-30 22:39:06 +0200

    add treemap sprites and implement them in the treemap scene

--- Assets/Scripts/GameplayScript.cs
+++ Assets/Scripts/GameplayScript.cs
@@ -19,13 +19,13 @@ public class GameplayScript : MonoBehaviour
             StartCoroutine(House1Gameplay());
         } else if (SceneManager.GetActiveScene().name == "House2_Scene")
         {
-            
+            StartCoroutine(House2Gameplay());
         }
     }
 
+    // raum 1:
     private IEnumerator House1Gameplay()
     {
-        prologScript.prologPanel.SetActive(true);
         // Prolog abspielen --> monologbar mit Cape-Main-Charakter im hintergrund
         yield return StartCoroutine(prologScript.ShowProlog(dialogLine[0]));
 
@@ -33,27 +33,31 @@ public class GameplayScript : MonoBehaviour
         // Direkt dialog mit kindergartenfee die baum abstaubt
         dialogScript.ShowDialogueWithoutButtons(dialogLine[1]);
         // kindergardenFairy.GetComponent<SpriteRenderer>().sprite = ;
-        // InputBlocker.Instance.BlockInput(); // --> evtl nicht nötig, kann auch durch monolog gesperrt werden
-
+        // --> ella danach nicht mehr anklickbar --> clickable character script entfernen
+        // dann frei rumbewegen
+        // rock paper scissors kind MUSS angeklickt werden --> checken
+        // danach wieder dialog mit ella --> man bekommt LEERE tasse in inventar
+        // dialogScript.ShowDialogueWithoutButtons(dialogLine[2]);
+        // GameObject.Find("ItembarManager").GetComponent<ItembarScript>().add_item(/*leere tasse*/);
+        // danach kaffeekanne finden und anklicken --> kaffee wird in tasse aufgefüllt
+        // wieder dialog mit ella (tasse zurückbringen) --> rübergehen zu raum 2 
+        // dialogScript.ShowDialogueWithoutButtons(dialogLine[3]);
     }
-    // raum 1:
-    // --> ella danach nicht mehr anklickbar
-    // dann frei rumbewegen
-    // rock paper scissors kind MUSS angeklickt werden+
-    // danach wieder dialog mit ella --> man bekommt LEERE tasse in inventar
-    // danach kaffeekanne finden und anklicken --> kaffee wird in tasse aufgefüllt
-    // wieder dialog mit ella (tasse zurückbringen) --> rübergehen zu raum 2 
 
     // raum 2:
-    // dialog mit gartenfee
-    // rübergehen zu dusche
-    // --> auf dusche klicken --> sauber
-    // zurück zu dialog mit Gartenfee
-    // frei rumlaufen
-    // --> canopy kann nur gebaut werden, wenn 2 materialien im inventar vorhanden
-    // canopy bauen 
-    // zurück zu dialog mit gartenfee
-    // boden fegen
-    // wieder dialog mit gartenfee --> SCHLÜSSEl 1!!!
-    // blackscreen und credits --> done
+    private IEnumerator House2Gameplay()
+    {
+        // dialog mit gartenfee
+        // rübergehen zu dusche
+        // --> auf dusche klicken --> sauber
+        // zurück zu dialog mit Gartenfee
+        // frei rumlaufen
+        // --> canopy kann nur gebaut werden, wenn 2 materialien im inventar vorhanden
+        // canopy bauen 
+        // zurück zu dialog mit gartenfee
+        // boden fegen
+        // wieder dialog mit gartenfee --> SCHLÜSSEl 1!!!
+        // blackscreen und credits --> done
+        yield break;
+    }
 }
